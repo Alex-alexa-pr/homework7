cd3c87574a6f27dd18bec6aaeec78a9a206c468b
Oleksandra <primakovaaleksandra@gmail.com>
2022-08-10 21:09:29 +0200

    added function findShort

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 "use strict";
 
 //1
-function sumTo(n) {
-    if (n===1) {
-        return n;
+// function sumTo(n) {
+//     if (n===1) {
+//         return n;
+//     }
+//     return n + sumTo(n-1);
+// }
+// console.log(sumTo(4)); // 10
+// console.log(sumTo(3)); 
+
+//2
+function findShort(sentance) {
+    return sentance.split(' ').reduce((a, b) => (b.length > a.length) ? b : a);
     }
-    return n + sumTo(n-1);
-}
-console.log(sumTo(4)); // 10
-console.log(sumTo(3)); // 6
+    console.log(findShort("Lorem ipsum dolor sit amet"));
+    console.log(findShort("Hello world"));
+    console.log(findShort("Hi"));
+    console.log(findShort("She is David's sister"));
